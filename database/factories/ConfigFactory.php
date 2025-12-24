57557633e24557a908b56daf05525ff109f6f883
Binyamin Blatt <benjamin@deeptechshowcase.com>
2025-12-24 18:19:43 +0200

    Fix import statement for Factory in ConfigFactory.php

--- database/factories/ConfigFactory.php
+++ database/factories/ConfigFactory.php
@@ -1,12 +1,12 @@
 <?php
 
 use App\Models\Config as ConfigModel;
-// use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Database\Eloquent\Factory;
 use Faker\Generator as Faker;
 use Illuminate\Support\Carbon;
 
-/* @var $factory Factory */
 
+/* @var $factory Factory */
 $factory->define(ConfigModel::class, function (Faker $faker, array $attributes = []) {
     return [
         'name' => $attributes['name'] ?? $faker->word().$faker->asciify('*****'),
